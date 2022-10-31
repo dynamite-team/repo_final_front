fe18a30dfab896c678cf5f03b4b1f2b1c283a632
SoleSM <miliserrrano@gmail.com>
2022-10-31 14:54:58 -0300

    estadisticas view y mis prod

---
+++ src/components/graficos/BarGrap.jsx
@@ -0,0 +1,86 @@
+import React, { useMemo } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
+const scores = [6, 5, 2, 5, 3, 4, 6, 4, 5];
+const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const options = {
+  fill: true,
+  animations: false,
+  scales: {
+    y: {
+      min: 0,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      display: true,
+    },
+  },
+};
+
+const BarChart = () => {
+  const data = useMemo(function () {
+    return {
+      datasets: [
+        {
+          label: "Mis ventas",
+          tension: 0.3,
+          data: scores,
+          borderColor: "rgb(75, 192, 192)",
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        },
+      ],
+      labels,
+    };
+  }, []);
+
+  return (
+    <div>
+      <Bar data={data} options={options} />
+    </div>
+  );
+}
+
+export default BarChart;
--- src/views/vistaProductor/Estadisticas.jsx
+++ src/views/vistaProductor/Estadisticas.jsx
@@ -1,10 +1,33 @@
+import BarChart from "../../components/graficos/BarGrap";
+import NavBarBack from "../../components/navbars/NavBarBack";
+import styled from 'styled-components';
+
+const ContainerGrafico = styled.div`
+    align-items: stretch;
+    display: row;
+    margin:  2%  20%  2%  20%;
+`;
+const Title = styled.div`
+    font-size: 40px;
+    font-family: Cambria;
+    text-align: left;
+    margin-left: 5%;
+    margin-top: 2%
+`;
 
 const EstadisticasView = () => {
- return(
-    <>
-        <p>Vista Estadisticas</p>
-    </>
- )
+    return (
+        <>
+            <NavBarBack
+                color="navbar navbar-expand-lg bg-primary"
+                colorIcon="white" />
+            <Title>Estadísticas de sus ventas</Title>
+            <ContainerGrafico>
+                <BarChart />
+            </ContainerGrafico>
+
+        </>
+    )
 }
 
 export default EstadisticasView;
